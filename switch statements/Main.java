5b9bb61d5c6d2a8de22bbb5d35fbb4ac81665de0
Sid <deshmukhsiddhant420@gmail.com>
2023-02-07 10:45:14 +0530

    switch code

--- switch statements/Main.java
+++ switch statements/Main.java
@@ -1,8 +1,40 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-
-
         System.out.println("Using switch statements");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter an integer: ");
+        int day = sc.nextInt();
+        
+        
+
+switch (day) {
+  case 1:
+    System.out.println("Monday");
+    break;
+  case 2:
+    System.out.println("Tuesday");
+    break;
+  case 3:
+    System.out.println("Wednesday");
+    break;
+  case 4:
+    System.out.println("Thursday");
+    break;
+  case 5:
+    System.out.println("Friday");
+    break;
+  case 6:
+    System.out.println("Saturday");
+    break;
+  case 7:
+    System.out.println("Sunday");
+    break;
+  default:
+    System.out.println("Invalid day");
+}
+
 
 
     
