87bb291535928c579cff5f61dac92be78fdf4ed5
Sid <deshmukhsiddhant420@gmail.com>
2023-02-07 23:30:54 +0530

    FOR LOOP

---
+++ loops/forloop.java
@@ -0,0 +1,11 @@
+package loops;
+
+public class forloop {
+    public static void main (String[] args)
+    {
+        for (int i=0;i<6;i++){
+            System.out.println(i);
+        }
+
+    }
+}
