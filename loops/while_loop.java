9c0be76302ba450ad69e8c74c16bc9fe6578d27d
Sid <deshmukhsiddhant420@gmail.com>
2023-02-09 17:06:09 +0530

    while loops added

---
+++ loops/while_loop.java
@@ -0,0 +1,14 @@
+package loops;
+
+public class while_loop {
+     public static void main(String args[]) {
+        int i = 0;
+        while(i<=10)
+        {
+            System.out.println(i);
+            i++;
+        }
+        
+    }
+    
+}
