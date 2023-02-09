4678d7df835d3770d58554c8f28b7fdd036715af
Sid <deshmukhsiddhant420@gmail.com>
2023-02-09 17:03:20 +0530

    dowhile

--- loops/dowhile.java
+++ loops/dowhile.java
@@ -1,5 +1,14 @@
 package loops;
 
 public class dowhile {
-    
+    public static void main(String[] srgs)
+    {
+        System.out.println("Do while demonstration");
+        int i=0;
+        do{
+            System.out.println(i);
+            i++;
+        }while(i<9);
+        
+    }   
 }
